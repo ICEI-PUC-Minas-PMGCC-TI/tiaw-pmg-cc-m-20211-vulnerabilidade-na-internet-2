7d15f11a9a2efc2bc3d810317fa9849e3b678400
MeninoDave <84027438+MeninoDave@users.noreply.github.com>
2021-06-10 16:12:23 -0300

    Update searchbar.js

--- searchbar.js
+++ searchbar.js
@@ -1,37 +1,14 @@
-const lojaslist = document.getElementById('lojaslist');
-const searchBar = document.getElementById('searchbar');
-let lojas = [];
-searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
-    const filteredlojas = lojaslist.filter((loja) => {
-        return (
-            loja.name.toLowerCase().includes(searchString)
-        );
-    });
-    displaylojas(filteredlojas);
-});
-const loadlojas = async () => {
-    try {
-        const res = await fetch('LINK API HEROKU');
-        lojas = await res.json();
-        displaylojas(lojas);
-    } catch (err) {
-        console.error(err);
-    }
-};
-const displaylojas = (lojas) => {
-    const htmlString = lojas
-        .map((loja) => {
-            return `
-            <li class="loja">
-                <h2>${loja.nome}</h2>
-                <p>Comentários: ${loja.comentario}</p>
-                <img src="${loja.image}"></img>
-            </li>
-        `;
-    })
-    .join('');
-lojaslist.innerHTML = htmlString;
-};
+function Buscar_Loja() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('lojas');
 
-loadlojas();
+    for(i=0; i< x.length;i++){
+        if(!x[i].innerHTML.toLowerCase().includes(input)){
+            x[i].style.display="Resultado não encontrado :("
+        }else{
+            x[i].style.display="list-item"
+        }
+        
+    }
+}
